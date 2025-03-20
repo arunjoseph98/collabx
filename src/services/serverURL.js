a3f2752a2adb3ad72ce7e9c7f6d6d6691be30d25
arunjoseph98 <arunjosephcs98@gmail.com>
2025-03-20 11:28:18 +0530

    collabx v2.2

--- src/services/serverURL.js
+++ src/services/serverURL.js
@@ -1,2 +1,2 @@
 export const serverURL = "https://collabx-server.onrender.com"
-export const WebSocketURL = "ws://collabx-server.onrender.com/"
+export const WebSocketURL = "wss://collabx-server.onrender.com/"
